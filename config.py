adf594135b77604cf9bd58d90ebc7a3c18fbd2a5
kihuni <stephenkihuni55@gmail.com>
2023-09-13 18:17:11 +0300

    modifies config file

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import os
+
 class Config(object):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
--- init_db.py
+++ init_db.py
@@ -1,5 +1,12 @@
+import os
 from app import app, db
+from app.models import Person
+
+# Print the absolute path
+print("Expected path for site.db:", os.path.abspath("site.db"))
 
 with app.app_context():
     db.create_all()
+    table = Person.__table__
+    print(table)
     print("Database tables created!")
